5b69ba48a292f0cf8470982e84ef460daa763992
jimmyLi <liviu.c.simion@gmail.com>
2016-03-14 09:30:23 +0100

    renamed second page into home page

--- DinoMightApp/app/controllers/index.js
+++ DinoMightApp/app/controllers/index.js
@@ -33,7 +33,7 @@ function CheckLogin() {
 }
 
 function LoadPage2() {
-	var Var_ProductListWindow = Alloy.createController('dinomight').getView();
+	var Var_ProductListWindow = Alloy.createController('home').getView();
     // For Alloy projects, you can pass context
     // to the controller in the Alloy.createController method.
     // var win2 = Alloy.createController('win2', {foobar: 42}).getView();
