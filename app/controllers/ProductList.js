a7b53eed09fe4fd0fee463efed10a740f2d685b4
jimmyLi <liviu.c.simion@gmail.com>
2016-04-26 18:50:59 +0200

    removed app folder - added by mistake

--- app/controllers/ProductList.js
+++
@@ -1,8 +0,0 @@
-function LoadPage1() {
-	var Var_Index = Alloy.createController('home').getView();
-    Var_Index.open();
-}
-
-function doClick(e) {
-    alert("You clicked!");
-}
--- app/controllers/Register.js
+++
@@ -1,9 +0,0 @@
-function NewRegister() {
-	
-}
-function goBack(e) {
-	var backButton = Ti.UI.currentWindow;
-	backButton.addEventListener('android:back', function (e) {
-	    Ti.UI.fireEvent('backButton_back_event');
-	});
-}
